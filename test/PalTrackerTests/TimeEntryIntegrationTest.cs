3c0637984c4d66a38f20b83870be7282df77a45a
Mike Gehard <mgehard@pivotal.io>
2018-06-13 09:14:20 -0600

    MYSQL Is ready...

--- test/PalTrackerTests/TimeEntryIntegrationTest.cs
+++ test/PalTrackerTests/TimeEntryIntegrationTest.cs
@@ -19,6 +19,9 @@ namespace PalTrackerTests
         public TimeEntryIntegrationTest()
         {
             _testClient = IntegrationTestServer.Start().CreateClient();
+            Environment.SetEnvironmentVariable("MYSQL__CLIENT__CONNECTIONSTRING", DbTestSupport.TestDbConnectionString);
+            DbTestSupport.ExecuteSql("TRUNCATE TABLE time_entries");
+
         }
 
         [Fact]
